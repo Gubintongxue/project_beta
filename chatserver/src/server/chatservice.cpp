196dd97446848015b37c6c2388ee0278056b7cf4
gubintongxue <1205408034@qq.com>
2023-09-10 15:44:27 +0800

    点对点聊天业务处理 -toid在线转发消息，服务器主动推送给toid用户

--- chatserver/src/server/chatservice.cpp
+++ chatserver/src/server/chatservice.cpp
@@ -18,7 +18,7 @@ ChatService::ChatService()
     // 用户基本业务管理相关事件处理回调注册
     _msgHandlerMap.insert({LOGIN_MSG, std::bind(&ChatService::login, this, _1, _2, _3)});
     _msgHandlerMap.insert({REG_MSG, std::bind(&ChatService::reg, this, _1, _2, _3)});
-
+    _msgHandlerMap.insert({ONE_CHAT_MSG, std::bind(&ChatService::oneChat, this, _1, _2, _3)});
 }
 
 
@@ -144,4 +144,26 @@ void ChatService::clientCloseException(const TcpConnectionPtr &conn)
         user.setState("offline");
         _userModel.updateState(user);
     }
+}
+
+// 一对一聊天业务
+void ChatService::oneChat(const TcpConnectionPtr &conn, json &js, Timestamp time)
+{
+    int toid = js["toid"].get<int>();
+
+    {
+        lock_guard<mutex> lock(_connMutex);
+        auto it = _userConnMap.find(toid);
+        if (it != _userConnMap.end())
+        {
+            // toid在线，转发消息   服务器主动推送消息给toid用户
+            it->second->send(js.dump());
+            return;
+        }
+    }
+
+    // 查询toid是否在线 
+
+    // toid不在线，存储离线消息
+    
 }
